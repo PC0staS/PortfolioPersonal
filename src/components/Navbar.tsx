2ed99b044c861b39d27d338eabab9e50572fab24
Pablo Costas <pablocostasnieto@gmail.com>
2025-09-13 18:03:41 +0200

    refactor: Update demo link in FileTransfer MDX and enhance ProyectoCards component with improved structure and accessibility

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center p-4 bg-[#1e1e1e] text-white px-50">
-      <div className="text-lg font-bold">Explora mi Portafolio</div>
+      <div className="text-lg font-bold">Explora mi Portfolio</div>
       <div className="space-x-4">
         <Link scroll href={'/' + '#proyectos'} className="hover:underline">Proyectos</Link>
         <Link scroll href={'/' + '#experiencia'} className="cursor-not-allowed">Experiencia</Link>
