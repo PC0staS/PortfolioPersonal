e4913dc0c0452c650077bfa41dc6fd2262d3fc05
Pablo Costas <pablocostasnieto@gmail.com>
2025-09-13 16:55:17 +0200

    style: Update CSS for view transitions and enhance layout structure; add navigation links

--- src/app/proyectos/[id]/page.tsx
+++ src/app/proyectos/[id]/page.tsx
@@ -2,7 +2,7 @@
 import { getProyectos } from "@/lib/proyectos";
 import ProyectoHeroImage from "@/components/ProyectoHeroImage";
 import RenderMDX from "@/components/renderMDX";
-
+import Link from "next/link"; 
 import "./mdx.css";
 import Navbar from "@/components/Navbar";
 
@@ -18,6 +18,9 @@ export default function ProyectoPage({ params }: { params: { id: string } }) {
     <div>
         <Navbar />
         <div className="max-w-3xl mx-auto mt-12 p-4">
+          <Link href={`/` + '#proyectos'}>
+                <span className="text-gray-400 ">Volver a los proyectos.</span>
+              </Link>
             <div className="flex flex-col items-center">
             <ProyectoHeroImage src={proyecto.heroImage} alt={proyecto.title}  />
             <h1 className="text-4xl font-bold mb-8 text-center">{proyecto.title}</h1>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@ export default function Navbar() {
     <nav className="flex justify-between items-center p-4 bg-[#1e1e1e] text-white px-50">
       <div className="text-lg font-bold">Explora mi Portafolio</div>
       <div className="space-x-4">
-        <a href="#about" className="hover:underline">Sobre mí</a>
-        <a href="#projects" className="hover:underline">Proyectos</a>
-        <a href="#contact" className="hover:underline">Contacto</a>
+        <a href={`/` + '#proyectos'} className="hover:underline">Proyectos</a>
+        <a href={`/` + '#experiencia'} className="cursor-not-allowed">Experiencia</a>
+        <a href={`/` + '#contacto'} className="hover:underline">Contacto</a>
       </div>
     </nav>
   );
