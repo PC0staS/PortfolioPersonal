8663e77895d3c799dcc61ac67d589970abdc435e
PC0staS <pablocostasnieto@gmail.com>
2025-09-24 18:01:37 +0200

    feat: add GSAP animations to MainScreen component for enhanced text effects

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,9 +1,39 @@
 "use client";
 
 import Image from "next/image";
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(SplitText);
+gsap.registerPlugin(useGSAP);
 
 export default function MainScreen() {
   const cv = "/cv.pdf";
+
+  useGSAP(() => {
+    const splitSobreMi = new SplitText("#sobre-mi", { type: "chars" });
+    const splitNombre = new SplitText("#nombre", { type: "chars" });
+
+    const tl = gsap.timeline();
+
+    tl.from(splitNombre.chars, {
+      duration: 0.8,
+      opacity: 0,
+      y: 80,
+      ease: "back.out(1.7)",
+      stagger: 0.05,
+    }).from(
+      splitSobreMi.chars,
+      {
+        duration: 0.1,
+        opacity: 0,
+        stagger: 0.03,
+      },
+      "-=0.4"
+    );
+  });
+
   return (
     <div className="flex flex-col items-center mt-10 px-4">
       <Image
@@ -13,10 +43,16 @@ export default function MainScreen() {
         height={200}
         className="rounded-full border-3 border-white"
       />
-      <h1 className="text-xl sm:text-2xl font-extrabold mt-4 text-center">
+      <h1
+        id="nombre"
+        className="text-xl sm:text-2xl font-extrabold mt-4 text-center"
+      >
         ¡Hola! Soy Pablo Costas
       </h1>
-      <h2 className="text-base sm:text-lg mt-4 text-gray-600 dark:text-gray-400 text-center max-w-3xl leading-relaxed px-2">
+      <h2
+        id="sobre-mi"
+        className="text-base sm:text-lg mt-4 text-gray-600 dark:text-gray-400 text-center max-w-3xl leading-relaxed px-2"
+      >
         Estoy estudiando Administración de Sistemas Informáticos en Red. <br />
         Además, me encanta el desarrollo de aplicaciones web y multiplataforma.{" "}
         <br />
@@ -38,6 +74,7 @@ export default function MainScreen() {
           Descarga mi CV
         </a>
       </div>
+
     </div>
   );
 }
