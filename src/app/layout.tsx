091e9556c18c4296e9dbca368a8c78b764a19f63
Pc0stas <pablocostasnieto@gmail.com>
2026-03-19 18:46:40 +0100

    feat: add @vercel/analytics package and integrate Analytics component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://pablocostas.dev"),
@@ -103,6 +104,7 @@ export default function RootLayout({
         {children}
       </body>
       <SpeedInsights />
+      <Analytics />
     </html>
   );
 }
